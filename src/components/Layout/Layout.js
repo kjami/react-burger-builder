910a6123dc15936f95858831c59763fe5f1a3907
kjami <kishorjami@gmail.com>
2019-09-06 18:05:32 +0800

    Added dummy nav items and logo.

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 import classes from './Layout.module.css';
 
 const layout = (props) => (
     <React.Fragment>
-        <div className={classes.Content}>Toolbar, Sidebar, Backdrop</div>
-        <main>
+        <Toolbar />
+        <main className={classes.Content}>
             {props.children}
         </main>
     </React.Fragment>
